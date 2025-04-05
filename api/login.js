bcdf2e896dc77071c8e3021d6c5104f4cca7f688
cudamsamsex <phanhoailink@gmail.com>
2025-04-05 10:58:54 +0700

    Update and rename check-login.js to login.js

---
+++ api/login.js
@@ -0,0 +1,30 @@
+// api/login.js
+const accounts = {
+    admin: {
+        password: "phanhoangna",
+        netflixAccount: {
+            username: "netflixuser",
+            password: "netflixpass",
+            profiles: [
+                { name: "Profile 1", password: "1111" },
+                { name: "Profile 2", password: "0202" },
+                { name: "Profile 3", password: "1357" },
+                { name: "Profile 4", password: "4488" },
+                { name: "Profile 5", password: "0055" },
+            ],
+        },
+    },
+};
+
+export default function handler(req, res) {
+    if (req.method === "POST") {
+        const { username, password } = req.body;
+        if (accounts[username] && accounts[username].password === password) {
+            res.status(200).json(accounts[username].netflixAccount);
+        } else {
+            res.status(401).json({ message: "Invalid username or password" });
+        }
+    } else {
+        res.status(405).json({ message: "Method Not Allowed" });
+    }
+}
